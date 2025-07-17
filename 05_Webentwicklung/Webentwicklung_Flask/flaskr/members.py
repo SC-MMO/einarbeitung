e94c1322116e0366f2ef7ae4bd9d4cf234fc294b
moritz.mummert <moritz.mummert@scaltel.de>
2025-07-17 08:18:17 +0200

    Added flaskwtf form to create/edit members (only display for now

--- 05_Webentwicklung/Webentwicklung_Flask/flaskr/members.py
+++ 05_Webentwicklung/Webentwicklung_Flask/flaskr/members.py
@@ -1,7 +1,17 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 from .db import exec_cmd, read_rows, read_columns
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Length
 
 member_bp = Blueprint('members', __name__, url_prefix='/members')
+
+class MemberForm(FlaskForm):
+    name = StringField('name')
+    squadName = StringField('squadName')
+    age = StringField('age')
+    secretIdentity = StringField('secretIdentity')
+    submitField = SubmitField("Submit")
 
 @member_bp.route('/', methods=["GET", "POST"])
 def show_members():
@@ -24,7 +34,6 @@ LEFT JOIN
 GROUP BY 
     members.memberID
 ORDER BY members.memberID;
-                   
 """)
 
     columns = ["Name", "Age", "Secret Identity", "Status", "Squad Name", "Active", "Powers"]
@@ -46,3 +55,29 @@ def show_a_member(memberName):
     res["powers"] = powers
 
     return render_template("member.html", res=res)
+
+
+@member_bp.route('/create', methods=["GET", "POST"])
+def create_a_member():
+    form = MemberForm(request.form)
+    if request.method == "POST" and form.validate():
+        
+        return redirect(url_for('index'))
+    return render_template("edit_member.html", form=form, intent='create', table='members')
+
+
+@member_bp.route('/<memberName>/edit', methods=["GET", "POST"])
+def edit_a_member(memberName):
+    member_data = read_rows(table="members", columns=['squadId', 'age', 'secretIdentity'], limit=1, filter_args=[f"members.name = '{memberName}'"])[0]
+    squad_name = read_rows(table="squads", columns=['squadName'], limit=1, filter_args=[f"squads.squadId = '{member_data[0]}'"])[0][0]
+
+    form = MemberForm(request.form)
+    form.name.data = memberName
+    form.squadName.data = squad_name
+    form.age.data = member_data[1]
+    form.secretIdentity.data = member_data[2]
+
+    if request.method == "POST" and form.validate():
+        
+        return redirect(url_for('index'))
+    return render_template("edit_member.html", form=form, intent='Edit', table='member')
