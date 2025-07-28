974fcdc6713a58147833ca90f572a4ef12eb5e75
moritz.mummert <moritz.mummert@scaltel.de>
2025-07-28 16:33:47 +0200

    Testscript aktualisiert

--- 07_API/testscript.py
+++ 07_API/testscript.py
@@ -1,10 +1,27 @@
 import requests
+from typing import Union
 
-url = "http://127.0.0.1:5001/api/squads"
-response = requests.get(url)
+def exec_get(*, url:str) -> Union[list, dict, str]:
+    response = requests.get(url)
+    try:
+        return response.json()
+    except ValueError:
+        return f"Response was not JSON: {response.text}"
 
-print(f"GET {url}: {response.status_code}")
-try:
-    print(response.json())
-except ValueError:
-    print("Response was not JSON:", response.text)
+api_urls=[
+    "http://127.0.0.1:5001/api/squads",
+    "http://127.0.0.1:5001/api/members",
+    "http://127.0.0.1:5001/api/powers",
+    "http://127.0.0.1:5001/api/squads/1",
+    "http://127.0.0.1:5001/api/members/10",
+    "http://127.0.0.1:5001/api/powers/1000" #Will return {"error": "404 Not Found: Object not found"} cause we dont have that many powers
+]
+reqs = [exec_get(url=api_url) for api_url in api_urls]
+
+import json
+for req in reqs:
+    if isinstance(req, str):
+        print(req)
+    else:
+        print(json.dumps(req, indent=4))
+    print("\n\n\n")
