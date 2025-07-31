89c4c9b5728f645ef3aaf32211302510a1fce6cf
moritz.mummert <moritz.mummert@scaltel.de>
2025-07-31 09:01:06 +0200

    Filteroption hinzugefügt

--- 09_JavaScript/Webentwicklung_Flask/flaskr/static/data_table/data_table.js
+++ 09_JavaScript/Webentwicklung_Flask/flaskr/static/data_table/data_table.js
@@ -1,3 +1,13 @@
+function filter_data(data_, filter) {
+  let res = [];
+  filter = filter.toLowerCase();
+  data_.forEach(item => {
+    if (item.some(element => String(element).toLowerCase().includes(filter)))
+      res.push(item);
+  });
+  return res;
+}
+
 function createTableHead(table, data) {
   const thead = document.createElement("thead");
   const headerRow = document.createElement("tr");
@@ -31,20 +41,25 @@ function createTableBody(table, data) {
 }
 
 function loadTable(count, page_) {
-  
   page = page_
   $.ajax({
     url: './test',
     method: 'GET',
     dataType: 'json',
     success: function (response) {
-      const data = response;
+      let data = response;
+
       const objectTable = document.getElementById("objectTable");
       objectTable.innerHTML = "";
+      
       let table = document.createElement("table");
       table.className = "display";
 
       table = createTableHead(table, data[0]);
+      
+      if (filter) {
+        data[1] = filter_data(data[1], filter)
+      }
 
       const start = (page - 1) * count;
       const end = page * count;
@@ -60,34 +75,33 @@ function loadTable(count, page_) {
 var count_val = document.getElementById('entrycount').value;
 var page = 1;
 var page_count;
+var filter = ""
 
 $(document).ready(function () {
-  reloadTable(count_val, 1)
+  reloadTable(1, count_val)
 });
 
-function reloadTable(count, page_=1 ) {
+function reloadTable(page_=1, count=count_val) {
   count_val = count;
   page = page_
-  loadTable(count_val, page)
+  loadTable(count_val, page, filter)
 }
 
 function first_page() {
   if (page!=1) {
     page = 1;
-    reloadTable(count_val, page)
+    reloadTable(page, count_val)
   }
 }
 
 function previous_page() {
   if (page>1) {
     page-=1
-    reloadTable(count_val, page)
+    reloadTable(page, count_val)
   }
 }
 
-function build_pagination_pages() {  
-  const container = document.getElementById('pageButtonContainer');
-  container.innerHTML = "";
+function handle_pagination_pages() {
   let paginationPages = Array.from({length: page_count}, (x, i) => i+1);
   
   if (page_count<= 5 | page < 3) {
@@ -99,31 +113,48 @@ function build_pagination_pages() {
   else {
     paginationPages = paginationPages.slice(page-3, page+2);
   }
-  
-  paginationPages.forEach(paginationPage => {
-    const button = document.createElement('button');
+  return paginationPages
+}
+
+function create_pagination_button(paginationPage) {
+  const button = document.createElement('button');
     button.textContent = `${paginationPage}`;
     button.id = `btn${paginationPage}`;
     if (paginationPage == page)
       button.style = `color:red;`
 
     button.addEventListener('click', () => {
-      reloadTable(count_val, paginationPage)
+      reloadTable(paginationPage, count_val)
     });
-    container.appendChild(button);
+    return button
+}
+
+function build_pagination_pages() {  
+  const container = document.getElementById('pageButtonContainer');
+  container.innerHTML = "";
+
+  const paginationPages = handle_pagination_pages()
+  
+  paginationPages.forEach(paginationPage => {
+    container.append(create_pagination_button(paginationPage))
   });
 }
 
 function next_page() {
   if (page<page_count) {
     page+=1
-    reloadTable(count_val, page)
+    reloadTable(page, count_val)
   }
 }
 
 function last_page() {
   if (page != page_count) {
     page = page_count
-    reloadTable(count_val, page)
+    reloadTable(page, count_val)
   }
+}
+
+function filter_change(newFilter) {
+  filter = newFilter
+  reloadTable(1, count_val)
 }
